b0bb2959fc73c85aa1f8ccdc4ae6c4be34f65eb1
kdhaywar <kdhaywar@ualberta.ca>
2015-11-28 19:47:21 -0700

    kyle push?
    Much faster. removed hog extraction from loop as to not repeat
    computations

--- svm/svmrun.m
+++ svm/svmrun.m
@@ -7,22 +7,30 @@
 %%%%TODO I think this svm would work better if the images had some padding
 %%%%readMnist function removes it so it would be best to readd some or
 %%%% modify readmnist
-
+clear all;
 imgFileTrain = 'train-images.idx3-ubyte';
 labelFileTrain = 'train-labels.idx1-ubyte';
 imgFileTest = 't10k-images.idx3-ubyte';
 labelFileTest = 't10k-labels.idx1-ubyte';
-numTrain = 1000;
-numTest = 100;
+numTrain = 5000;
+numTest = 10000;
 offsetTrain = 0;
 offsetTest = 0;
 amin = 0; %min grayscale cutoff
 amax = 1; %max grayscale cutoff
 hogCellSz = 4;
 
-
+% if exist('svmimages.mat', 'file') == 2
+%     load('svmimages');
+% else
+%     [imgTrain, labelsTrain] = readMNIST(imgFileTrain, labelFileTrain, numTrain, offsetTrain);
+%     [imgTest, labelsTest] = readMNIST(imgFileTest, labelFileTest, numTest, offsetTest);
+%     save('svmimages.mat','imgTrain','labelsTrain','imgTest','labelsTest');
+% end
 [imgTrain, labelsTrain] = readMNIST(imgFileTrain, labelFileTrain, numTrain, offsetTrain);
 [imgTest, labelsTest] = readMNIST(imgFileTest, labelFileTest, numTest, offsetTest);
+
+
 imgTrain = mat2gray(imgTrain, [amin amax]);
 imgTest = mat2gray(imgTest, [amin amax]);
 
@@ -37,29 +45,37 @@ hogFeatureSize = prod([BlocksPerImage, BlockSize, NumBins]);
 
 
 % Train an SVM classifier for each digit
-for d = 0:9
+numTrainingImages = size(imgTrain,3);
+trainingFeatures  = zeros(numTrainingImages,hogFeatureSize,'single');
+for i = 1:numTrainingImages
+    trainingFeatures(i,:) = extractHOGFeatures(imgTrain(:,:,i),'CellSize',cellSize);
+end
 
+
+for d = 0:9
+    fprintf('Training digit%d.\n',d);
     % Pre-allocate trainingFeatures array
-    numTrainingImages = size(imgTrain,3);
-    trainingFeatures  = zeros(numTrainingImages,hogFeatureSize,'single');
+
 
     % Extract HOG features from each training image. trainingImages
     % contains both positive and negative image samples.
-    for i = 1:numTrainingImages
-        %imgTrain = imread(trainingImages{i,d});
 
-        %imgTrain = preProcess(imgTrain);
-
-        trainingFeatures(i,:) = extractHOGFeatures(imgTrain(:,:,i),'CellSize',cellSize);
-    end
 
     % Train a classifier for a digit. Each row of trainingFeatures contains
     % the HOG features extracted for a single training image. The
     % trainingLabels indicate if the features are extracted from positive
     % (true) or negative (false) training images.
     trainGroundTruth = labelsTrain == d; 
-    options.MaxIter = 100000;
-    svm(d+1) = svmtrain(trainingFeatures, trainGroundTruth, 'Options',options);
+    options.MaxIter = 1000000;
+    boxCon = 500;
+    vioLvl = 0.5;
+    %svm(d+1) = svmtrain(trainingFeatures, trainGroundTruth, 'Options',options,'boxconstraint',boxCon,'kktviolationlevel',vioLvl,'kernel_function','rbf');
+    %svm(d+1) = svmtrain(trainingFeatures, trainGroundTruth,'kernel_function','rbf', 'RBF_Sigma', 3);
+    svm(d+1) = svmtrain(trainingFeatures, trainGroundTruth);
+    %svm(d+1) = fitcsvm(trainingFeatures,trainGroundTruth,'Standardize',true,'KernelFunction','RBF',...
+    %'KernelScale','auto');
+    %svm{d+1} = fitcsvm(trainingFeatures,trainGroundTruth);
+    
 end
 
 
