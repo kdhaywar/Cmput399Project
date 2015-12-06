a1df1ed8b9e603952e289ca88002d5fc021f8349
Sean Scheideman <searn@ualberta.ca>
2015-12-06 10:25:35 -0700

    added more cnn nets

--- cnn/cnn_mnist_experiments.m
+++ cnn/cnn_mnist_experiments.m
@@ -1,30 +1,31 @@
-%% Experiment with the cnn_mnist_fc_bnorm
+%Experiment with the cnn_mnist_fc_bnorm
+%[net_fc, info_fc] = cnn_mnist(@cnn_mnist_init_linear,'expDir', 'data/mnist-linear', 'useBnorm', false);
+[net_bn, info_bn] = cnn_mnist(@cnn_mnist_lenet,'expDir', 'data/mnist-lenet', 'useBnorm', false);
+%[net_fc, info_fc] = cnn_mnist(@cnn_mnist_init_test1,'expDir', 'data/mnist-test1', 'useBnorm', false);
+%[net_fc, info_fc] = cnn_mnist(@cnn_mnist_init_test2,'expDir', 'data/mnist-test2', 'useBnorm', false);
+%[net_fc, info_fc] = cnn_mnist(@cnn_mnist_init_test3,'expDir', 'data/mnist-test3', 'useBnorm', false);
 
-[net_bn, info_bn] = cnn_mnist(...
-  'expDir', 'data/mnist-bnorm', 'useBnorm', true);
-
-%[net_fc, info_fc] = cnn_mnist(...
-%  'expDir', 'data/mnist-baseline', 'useBnorm', false);
+%Experiments Tests with data augmentations 
+%TODO
 
 
-%%
-figure(1) ; clf ;
-subplot(1,2,1) ;
-semilogy(info_fc.val.objective, 'k') ; hold on ;
-semilogy(info_bn.val.objective, 'b') ;
-xlabel('Training samples [x10^3]'); ylabel('energy') ;
-grid on ;
-h=legend('BSLN', 'BNORM') ;
-set(h,'color','none');
-title('objective') ;
-subplot(1,2,2) ;
-plot(info_fc.val.error, 'k') ; hold on ;
-plot(info_fc.val.topFiveError, 'k--') ;
-plot(info_bn.val.error, 'b') ;
-plot(info_bn.val.topFiveError, 'b--') ;
-h=legend('BSLN-val','BSLN-val-5','BNORM-val','BNORM-val-5') ;
-grid on ;
-xlabel('Training samples [x10^3]'); ylabel('error') ;
-set(h,'color','none') ;
-title('error') ;
-drawnow ;
+% figure(1) ; clf ;
+% subplot(1,2,1) ;
+% semilogy(info_fc.val.objective, 'k') ; hold on ;
+% semilogy(info_bn.val.objective, 'b') ;
+% xlabel('Training samples [x10^3]'); ylabel('energy') ;
+% grid on ;
+% h=legend('BSLN', 'BNORM');
+% set(h,'color','none');
+% title('objective') ;
+% subplot(1,2,2) ;
+% plot(info_fc.val.error, 'k') ; hold on ;
+% %plot(info_fc.val.top5e, 'k--') ;
+% plot(info_bn.val.error, 'b') ;
+% %plot(info_bn.val.topFiveError, 'b--') ;
+% h=legend('BSLN-val','BSLN-val-5','BNORM-val','BNORM-val-5') ;
+% grid on ;
+% xlabel('Training samples [x10^3]'); ylabel('error') ;
+% set(h,'color','none') ;
+% title('error') ;
+% drawnow ;
---
+++ cnn/test_trained_cnn.m
@@ -0,0 +1,22 @@
+% Load the CNN learned before
+vl_setupnn;
+load('net-epoch-20.mat');
+[net_fc, info_fc] = cnn_mnist(@cnn_mnist_init_vgg,'expDir', 'data/mnist-vgg', 'useBnorm', true);
+n = 10;
+indices = randperm(70000,n);
+disp(indices);
+imdb = load('imdb.mat');
+% Load the sentence
+images = imdb.images.data(:,:,:,indices);
+labels = imdb.images.labels(indices);
+net.layers{end}.type = 'softmax';
+for i = 1:n
+    res = vl_simplenn(net, images(:,:,i));
+    scores = squeeze(res(end).x) ;
+    [bestScore, best] = max(scores) ;
+    fprintf(1,'Best score : %f, Digit : %d\n',bestScore,best-1);
+    %imshow(images(:,:,i));
+    %pause;
+end
+
+% Visualize the results
